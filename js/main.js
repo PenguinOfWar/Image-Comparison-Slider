af769fabac5c614e99203dcf81f4c58121593812
Darryl Walker <darryljwalker@gmail.com>
2015-09-18 14:08:52 +0100

    Tidying useragent detection.

--- js/main.js
+++ js/main.js
@@ -31,16 +31,16 @@ jQuery(document).ready(function($){
 		$.extend($.fn.disableTextSelect = function() {
             
             /* remove $.browser as it is deprecated in jQuery > 1.9 and replace it with navigator.userAgent */
-            var sUsrAg = navigator.userAgent;
-			return this.each(function(){
-				if( sUsrAg.indexOf("Firefox") > -1 ){//Firefox
-					$(this).css('MozUserSelect','none');
-				}else if( sUsrAg.indexOf("MSIE") > -1 ){//IE
-					$(this).bind('selectstart',function(){return false;});
-				}else{//Opera, etc.
-					$(this).mousedown(function(){return false;});
-				}
-			});
+            var userAgent = navigator.userAgent;
+    		return this.each(function(){
+    			if( userAgent.indexOf('Firefox') > -1 ){//Firefox
+    				$(this).css('MozUserSelect','none');
+    			}else if( userAgent.indexOf('MSIE') > -1 ){//IE
+    				$(this).bind('selectstart',function(){return false;});
+    			}else{//Opera, etc.
+    				$(this).mousedown(function(){return false;});
+    			}
+    		});
 		});
 		$('.noSelect').disableTextSelect();//No text selection on elements with a class of 'noSelect'
 	});
